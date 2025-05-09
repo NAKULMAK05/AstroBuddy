2682b9f29611a4968a33c8a2015f6432ce381b3e
Nakul Makode <134138075+NAKULMAK05@users.noreply.github.com>
2025-05-09 23:52:06 +0530

    Update helperfuncs.py

--- helperfuncs.py
+++ helperfuncs.py
@@ -3,8 +3,9 @@ import requests
 import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
+import numpy as np
+import pandas as pd
 
-chatllm = "opeanai-gpt-4o"
 llm = ChatGoogleGenerativeAI(
 model="gemini-1.0-pro",
 temperature=0, 
